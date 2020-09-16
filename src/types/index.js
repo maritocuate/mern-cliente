659c789f88d9e2473b9ca7e9cd6d59499e00f25a
marioquiroga <mquiroga@dosalcubo.com>
2020-09-16 02:40:40 -0300

    alerta context -> context, reducer, state

--- src/types/index.js
+++ src/types/index.js
@@ -12,3 +12,6 @@ export const ELIMINAR_TAREA = 'ELIMINAR_TAREA';
 export const ESTADO_TAREA = 'ESTADO_TAREA';
 export const TAREA_ACTUAL = 'TAREA_ACTUAL';
 export const EDITAR_TAREA = 'EDITAR_TAREA';
+
+export const MOSTRAR_ALERTA = 'MOSTRAR_ALERTA';
+export const OCULTAR_ALERTA = 'OCULTAR_ALERTA';
